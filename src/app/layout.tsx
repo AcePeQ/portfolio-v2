351a1413bf0d8c4010adca127377aead1f796ffc
AcePeQ <maciej.nojszewski22@gmail.com>
2026-05-16 17:13:20 +0200

    feat: enhance layout with Lato font integration and update global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,31 @@
+import { ReactNode } from "react"
+import { Lato } from "next/font/google"
 import "@/styles/globals.css"
+import { Metadata } from "next";
+
+const latoSans = Lato({
+  variable: "--font-lato-sans",
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+  display: "swap"
+});
+
+export const metadata: Metadata = {
+  title: "Maciej Nojszewski | Junior Frontend Developer",
+  description: "Personal portfolio website built with Next.js"
+}
 
 export default function Layout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: ReactNode
+}>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${latoSans.variable}`}>
       <body>
+        <header></header>
         <main>{children}</main>
+        <footer></footer>
       </body>
     </html>
   )
