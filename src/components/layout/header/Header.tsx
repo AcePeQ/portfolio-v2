93e3aa50a89b6b46cfbc1e352ec572bcde5df63e
AcePeQ <maciej.nojszewski22@gmail.com>
2026-05-19 18:34:16 +0200

    feat: enhance Button component with link support and additional variants, update Header layout

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,16 +1,19 @@
+import Button from "@/components/ui/button/Button"
 import Logo from "@/components/ui/logo/Logo"
 import MainNavigation from "@/components/ui/mainNavigation/MainNavigation"
 
 function Header() {
   return (
-    <header className="xl:w-full xl:h-15 flex items-center justify-between xl:gap-8">
-      <div className="xl:w-40 flex items-center justify-start">
+    <header className="w-full h-15 flex items-center justify-between gap-8">
+      <div className="w-40 flex items-center justify-start">
         <Logo />
       </div>
-      <div className="xl:flex-1">
+      <div className="flex-1">
         <MainNavigation />
       </div>
-      <div className="xl:w-40 flex items-center justify-end"></div>
+      <div className="w-40 flex items-center justify-end">
+        <Button isLink variant="primary" size="medium" path="/#contact">Hire Me</Button>
+      </div>
     </header>
   )
 }
--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,37 +1,44 @@
+import Link from "next/link";
 import { MouseEventHandler, ReactNode } from "react"
 
 type ButtonProps = {
-  isIcon: boolean;
-  variant: "primary" | "secondary" | "ghost";
-  size: "small" | "medium" | "big"
-  type: "submit" | "reset" | "button";
-  onClick: MouseEventHandler<HTMLButtonElement>;
+  isIcon?: boolean;
+  isLink?: boolean;
+  path?: string;
+  variant?: "primary" | "secondary" | "ghost" | "active";
+  size?: "small" | "medium" | "big"
+  type?: "submit" | "reset" | "button";
+  onClick?: MouseEventHandler<HTMLButtonElement>;
   children: ReactNode
 }
 
-function Button({ isIcon, variant, size, type, onClick, children }: ButtonProps) {
-  let clsx = "";
+function Button({ isLink = false, path, isIcon = false, variant = "primary", size = "medium", type = "button", onClick, children }: ButtonProps) {
+  let clsx = "flex items-center justify-center ";
 
   if (variant === "primary") {
-
+    clsx += "bg-orange-normal hover:bg-orange-normal-hover focus:bg-orange-normal-hover active:bg-orange-normal-active text-white "
   } else if (variant === "secondary") {
-
+    clsx += "bg-grey-normal hover:bg-grey-normal-hover focus:bg-grey-normal-focus active:bg-grey-normal-active text-white-normal hover:text-white-normal-hover focus:text-white-normal-hover active:text-white-normal-active "
   } else if (variant === "ghost") {
-
+    clsx += "bg-transparent text-white-dark hover:text-white-dark:hover focus:text-white-dark-hover active:text-white-dark-active border-2 border-white-dark hover:border-white-dark-hover focus:border-white-dark-hover active:border-white-dark-active "
+  } else if (variant === "active") {
+    clsx += "bg-orange-normal hover:bg-orange-normal focus:bg-orange-normal active:bg-orange-normal text-white "
   }
 
   if (size === "small") {
-
+    clsx += "px-10 py-3 rounded-lg text-lg font-bold ";
   } else if (size === "medium") {
-
+    clsx += "px-10 py-3 rounded-lg text-xl font-bold ";
   } else if (size === "big") {
-
+    clsx += "px-10 py-3.5 rounded-lg text-xl font-bold "
   }
 
   if (isIcon) {
+    clsx += "gap-2"
+  }
 
-  } else {
-
+  if (isLink && path) {
+    <Link href={path} className="clsx">{children}</Link>
   }
 
   return (
