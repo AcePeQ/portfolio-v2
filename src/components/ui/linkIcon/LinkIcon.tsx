e65668880a583aaf9f41cf9317e9b5fe9b51879d
AcePeQ <maciej.nojszewski22@gmail.com>
2026-05-20 16:48:47 +0200

    fix: remove width and height attributes from SVG icons for better responsiveness

--- src/components/ui/linkIcon/LinkIcon.tsx
+++ src/components/ui/linkIcon/LinkIcon.tsx
@@ -10,7 +10,7 @@ function LinkIcon({ path, children }: LinkIconProps) {
     <a className="w-10 h-10 flex items-center justify-center rounded-full border-2 border-white-darker bg-grey-normal 
     hover:bg-grey-normal-hover focus:bg-grey-normal-hover active:bg-grey-normal-active text-white-dark 
     hover:text-white-dark-hover focus:text-white-dark-hover active:text-white-dark-active" href={path}>
-      <span className="w-6 h-6 flex items-center justify-center">
+      <span className="w-6 h-6 flex items-center justify-center [&>svg]:w-full [&>svg]:h-full">
         {children}
       </span>
     </a>
