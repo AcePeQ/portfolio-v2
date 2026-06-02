41721d341e8f966b627a2a1a83ae7b0f050b8acd
AcePeQ <maciej.nojszewski22@gmail.com>
2026-06-02 15:32:24 +0200

    feat: add XIcon component and integrate it into MobileNavigation for menu toggle functionality

--- src/components/layout/header/components/MobileNavigation.tsx
+++ src/components/layout/header/components/MobileNavigation.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { MAIN_NAVIGATION } from '@/lib/constants/navigation'
-import { MenuIcon } from '@/lib/helpers/icons'
+import { MenuIcon, XIcon } from '@/lib/helpers/icons'
 import Link from 'next/link'
 import { useState } from 'react'
 
@@ -16,13 +16,14 @@ function MobileNavigation() {
   return (
     <>
       <div className="w-40 flex items-center justify-start lg:hidden">
-        <button onClick={toggleMenu} className="w-8 cursor-pointer text-white-dark-hover hover:text-white-dark active:text-white-dark-active focus:text-white-dark">
-          <MenuIcon />
+        <button onClick={toggleMenu} className="w-8 h-8 cursor-pointer text-white-dark-hover hover:text-white-dark 
+        active:text-white-dark-active focus:text-white-dark">
+          {isMenuOpen ? <XIcon /> : <MenuIcon />}
         </button>
       </div>
 
       {isMenuOpen &&
-        <div className="absolute z-20 h-[calc(100%-88px)] w-full top-22 left-0 bg-background">
+        <div className="absolute z-20 h-[calc(100%-88px)] w-full top-22 left-0 bg-background lg:hidden">
           <nav className='flex items-center justify-center w-full h-full'>
             <ul className='flex flex-col text-center gap-6'>
               {MAIN_NAVIGATION.map(link =>
